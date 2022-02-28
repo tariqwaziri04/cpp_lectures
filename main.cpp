b3b075b8f397f791e6b48b2b0f0116d352d373cb
Chatrapati Ākula <chatrapathi.akula@gmail.com>
2022-02-28 12:50:44 +0000

    simplified project structure

--- main.cpp
+++ main.cpp
@@ -1,13 +1,3 @@
 #include <cstdio>
-#include <optional>
-#include <string_view>
 
-struct Greeting
-{
-        static auto say() -> std::optional<std::string_view> { return {"Hello World\n"}; }
-};
-
-int main()
-{
-        if (const auto s = Greeting::say(); s) { std::printf("%s", s->data()); }
-}
+int main() { std::printf("%s\n", "Hello World"); }
