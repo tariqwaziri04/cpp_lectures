d03ce8db93caa726900a234723f61a789e94ec08
Chatrapati Ākula <chatrapathi.akula@gmail.com>
2022-02-28 13:49:39 +0000

    create sum of numbers program

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
-#include <array>
 #include <cstdio>
 
 int main()
 {
-        std::array<char, 32> name {};
-        std::printf("Enter your name: ");
-        std::scanf("%s", name.begin());
-        std::printf("%s\n", name.begin());
+    std::printf("Enter a list of numbers (0 to end): ");
+    int sum = 0;
+    int num = 0;
+
+    while (true)
+    {
+        std::scanf("%d", &num);
+        if (num == 0)
+        {
+            break;
+        }
+        sum += num;
+    }
+    std::printf("sum = %d\n", sum);
+
 }
