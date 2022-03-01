570f2fe91c905405636b74d4a5df799c9dc108fa
Chatrapati Ākula <chatrapathi.akula@gmail.com>
2022-03-01 10:27:46 +0000

    print even and odd elements of array

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
 #include <array>
 #include <cstdio>
 
-int main() {}
+int main()
+{
+        std::array<int, 5> x = {1, 2, 3, 4, 5};
+        for (int i = 0; i < x.size(); i += 1)
+        {
+                if (x[i] % 2 == 0) { std::printf("Even: x[%d] = %d\n", i, x[i]); }
+                else { std::printf("Odd: x[%d] = %d\n", i, x[i]); }
+        }
+
+        // Task: Add the even numbers in the array `x`.
+}
