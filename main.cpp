b2ea5f2f15cf90c683fac333daac31244e64f3aa
Chatrapati Ākula <chatrapathi.akula@gmail.com>
2022-03-01 09:45:45 +0000

    remove unused code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
-#include <cstdio>
 #include <array>
+#include <cstdio>
 
-int main()
-{
-    std::array<int, 5> arr = {1, 2, 3, 4, 5};
-    for (int i = 0; i < arr.size(); i += 1)
-    {
-        arr[i] *= 2;
-        std::printf("arr[%d] = %d\n", i, arr[i]);
-    }
-}
+int main() {}
