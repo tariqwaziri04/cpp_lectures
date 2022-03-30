11ed7f470747f8d0507718360c70d4bb9efb427e
tariqwaziri04 <tariqwaziri04@gmail.com>
2022-03-30 09:25:39 +0000

    remove unnecessary code

--- main.cpp
+++ main.cpp
@@ -1,17 +1,3 @@
-#include <array>
-#include <cstdio>
 
-int main()
-{
-        std::array<int, 5> x   = {1, 2, 3, 4, 5};
-        // for (int i = 0; i < x.size(); i += 1)
-        // {
-        //         if (x[i] % 2 == 0) { std::printf("Even: x[%d] = %d\n", i, x[i]); }
-        //         else { std::printf("Odd: x[%d] = %d\n", i, x[i]); }
-        // }
 
-        // Task: Add the even numbers in the array `x`.
-        int                sum = 0;
-        for (int i = 0; i < x.size(); i += 1) { sum = sum + x[i]; }
-        std::printf("sum = %d\n", sum);
-}
+int main() {}
